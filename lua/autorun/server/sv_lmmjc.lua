a3efa2f08ae7ee3cc50dd28d1c0c6428d509d5ce
Matt S <XxLMM13xXgaming@gmail.com>
2016-01-10 00:27:28 -0500

    Fixed playing for all error

--- lua/autorun/server/sv_lmmjc.lua
+++ lua/autorun/server/sv_lmmjc.lua
@@ -4,12 +4,14 @@ resource.AddSingleFile("sound/lmmjc_JOHNCENA.wav")
 
 function JohnCenaHasReallyJoinedTheServer( ply )
 	timer.Simple( 3, function()
-		if !ply:IsValid() and !ply:IsAdmin() then
+		if !ply:IsValid() then
 			return
 		end
-		for k,v in pairs(player.GetAll()) do
-			net.Start("LMMJCHasJoined")
-			net.Send(v)
+		if ply:IsAdmin() then
+			for k,v in pairs(player.GetAll()) do
+				net.Start("LMMJCHasJoined")
+				net.Send(v)
+			end
 		end
 	end)
 end
